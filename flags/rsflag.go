f4dfa4fa229eefa341d73f4094fca4563475fdbe
Reason <Reason@Lee.com>
2019-10-21 10:27:18 +0800

    flag

---
+++ flags/rsflag.go
@@ -0,0 +1,26 @@
+package rsflag
+
+import (
+	"flag"
+	"fmt"
+)
+
+// FlagTest test Flag func
+func FlagTest() {
+	config := flag.String("c", "./config/wallet-sign.yaml", "full path config file")
+	_ = flag.Bool("gen", false, "gen currency address")
+	_ = flag.String("hash", "", "hash for given address")
+	_ = flag.Bool("pgen", false, "gen protected currency address")
+	_ = flag.Bool("update", false, "update address hash of given chain in config")
+	_ = flag.String("genPrivKey", "", "get the private key of the given address")
+	flag.Parse()
+
+	cmdParam := make(map[string]string, 0)
+	flag.Visit(func(i *flag.Flag) {
+		cmdParam[i.Name] = i.Value.String()
+		fmt.Printf("%s: %s \n", i.Name, i.Value.String())
+	})
+
+	fmt.Println(cmdParam)
+	fmt.Println(config)
+}
--- main.go
+++ main.go
@@ -1,37 +1,12 @@
 package main
 
 import (
-	// rsredis "github.com/reason/redis"
-
 	_ "github.com/reason/context"
-	rsrabbit "github.com/reason/rabbitMQ"
+	rsflag "github.com/reason/flags"
+	_ "github.com/reason/rabbitMQ"
+	_ "github.com/reason/redis"
 )
 
 func main() {
-
-	rsrabbit.Send("ReasonMQ")
-
-	// rsredis.RedioBasicGetSet()
-	// fmt.Println()
-	// rsredis.RedioExpirationGetSet()
-	// fmt.Println()
-	// rsredis.RedioCheckExist()
-	// fmt.Println()
-	// rsredis.RedioDeleteKey()
-	// fmt.Println()
-	// rsredis.RedioJSONValue()
-	// fmt.Println()
-	// rsredis.RedioAppendExpiration()
-	// fmt.Println()
-	// rsredis.RedioListValue()
-
-	// rsredis.RedisBasicUsage()
-
-	// rscontext.CTWithCancel()
-	// fmt.Println()
-	// rscontext.CTWithDeadline()
-	// fmt.Println()
-	// rscontext.CTWithTimeout()
-	// fmt.Println()
-	// rscontext.CTWithValue()
+	rsflag.FlagTest()
 }
